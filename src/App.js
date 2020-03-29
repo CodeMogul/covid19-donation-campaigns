2ba2ce153ba8ceebd9b2b63a1496a8e13b1d522c
Siddhesh Nachane <siddhesh.nachane@bizongo.com>
2020-03-29 20:06:13 +0530

    (feat): Initial build complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CampaignList from './CampaignList';
+import { HeartFilled } from '@ant-design/icons';
+
+function Header(props) {
+  return (
+    <header className="app-header-footer-common app-header">
+      Covid-19 Donation Campaigns
+    </header>
+  )
+}
+
+function Footer(props) {
+  return (
+    <div className="app-header-footer-common app-footer">
+      <p>Made with <HeartFilled style={{ color: 'red' }} /> by <a href="https://github.com/codemogul">Siddhesh Nachane</a></p>
+      <p>We are under no circumstances affiliated to any of the above listed organisation or campaigns. And try to maintain the latest available information updates on our own.</p>
+      <p>If you would like to add your campaigns to the list, or report any of the above campaigns, or report a change of info, <a href="mailto:siddesh.nachane@outlook.com">write to us.</a></p>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <CampaignList />
+      <Footer />
     </div>
   );
 }
---
+++ src/CampaignList.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+import { Button, List } from 'antd';
+import Avatar from 'antd/es/avatar';
+// import List from 'antd/es/list';
+import Descriptions from 'antd/es/descriptions';
+import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
+import 'antd/es/list/style/css';
+import 'antd/es/avatar/style/css';
+import 'antd/es/descriptions/style/css';
+import 'antd/es/button/style/css';
+
+import './CampaignList.css';
+
+import listData from './campaigns.json';
+
+const IconText = ({ icon, text }) => (
+  <span>
+    {React.createElement(icon, { style: { marginRight: 8 } })}
+    {text}
+  </span>
+);
+
+export default class CampaignList extends Component {
+  render() {
+    return (
+      <div className="list-container">
+        <List
+          itemLayout="vertical"
+          size="large"
+          pagination={{
+            onChange: page => {
+              console.log(page);
+            },
+            pageSize: 10,
+          }}
+          dataSource={listData}
+          renderItem={({ initiator, ...item }) => (
+            <List.Item
+              key={item.name}
+              // actions={[
+              //   <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
+              //   <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
+              //   <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
+              // ]}
+              extra={(
+                <a href={item.link}>
+                  <img className="cover-image" alt="logo" src={item.image_url} />
+                </a>
+              )}
+              className="list-item"
+            >
+              <List.Item.Meta
+                avatar={<Avatar src={item.avatar_url} />}
+                title={<a href={item.link}>{item.name}</a>}
+                description={item.description}
+              />
+
+              <div className="item-content-container">
+
+                <Descriptions column={2} bordered size="small" style={{ flexBasis: '80%' }}>
+                  <Descriptions.Item label="An Initiative By">
+                    <a href={initiator.link}>{initiator.name}</a>
+                  </Descriptions.Item>
+                  {/* <Descriptions.Item label="Remark">empty</Descriptions.Item> */}
+                  <Descriptions.Item label="NGO Partner">
+                    <a href={item.partner_ngo.link}>{item.partner_ngo.name}</a>
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Platform Partner">
+                    <a href={item.partner_platform.link}>{item.partner_platform.name}</a>
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Tax Exempted u/s 80G">{item.tax_exemption ? "Yes" : "No"}</Descriptions.Item>
+                  <Descriptions.Item label="Category">{item.category}</Descriptions.Item>
+                  <Descriptions.Item label="Location">{item.location}</Descriptions.Item>
+                </Descriptions>
+
+                <Button type="primary" href={item.link}>Know More</Button>
+              </div>
+
+            </List.Item>
+          )}
+        />
+      </div>
+    )
+  }
+}
