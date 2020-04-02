a3de9cb9c810215a49944492be318e4360dcf4e0
Siddhesh Nachane <siddhesh.nachane@bizongo.com>
2020-04-02 15:35:59 +0530

    (chore): Add banner - Add banner - Update css - Update bizongo banner url

--- src/App.js
+++ src/App.js
@@ -15,18 +15,20 @@ listData.forEach(item => {
 
 function Header(props) {
   return (
-    <header className="app-header-footer-common app-header">
-      Covid-19 Donation Campaigns
-    </header>
+    <img src="/COVID-19_banner_with_text.png" className="banner" alt="banner" />
   )
 }
 
 function Footer(props) {
   return (
     <div className="app-header-footer-common app-footer">
-      <p>Made with <HeartFilled style={{ color: 'red' }} /> by <a href="https://github.com/codemogul">Siddhesh Nachane</a></p>
+      <p>Made with <HeartFilled style={{ color: 'red' }} /></p>
       <p>We are under no circumstances affiliated to any of the above listed organisation or campaigns. And try to maintain the latest available information updates on our own.</p>
-      <p style={{ marginBottom: 0 }}>If you would like to add your campaigns to the list, or report any of the above campaigns, or report a change of info, <a href="mailto:siddesh.nachane@outlook.com">write to us.</a></p>
+      <p style={{ marginBottom: 0 }}>
+        If you face any issues, want to report any campaigns, report a change of info or would like to add your campaigns to the list
+        {" "}
+        <a target="_blank" rel="noopener noreferrer" href="https://forms.gle/sqiGGVRta3GJv9fE9">write to us.</a>
+      </p>
     </div>
   )
 }
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Select } from 'antd';
+import { Select, Divider } from 'antd';
 import 'antd/es/select/style/css';
+import 'antd/es/divider/style/css';
 
 import './Filter.css'
 
@@ -9,32 +10,38 @@ const { Option } = Select;
 
 export default function Filter(props) {
   return (
-    <div className="filter-container">
-      <label htmlFor="categories-select">Categories</label>
-      <Select
-        mode="multiple"
-        placeholder="Please select"
-        onChange={props.setCategories}
-        tokenSeparators=","
-        className="dropdown"
-      >
-        {
-          props.categories.map(category => <Option key={category} value={category}>{category}</Option>)
-        }
-      </Select>
+    <>
+      <Divider orientation="left">Filter By</Divider>
+      <div className="filter-container">
+        <label htmlFor="categories-select">Categories</label>
+        <Select
+          mode="multiple"
+          placeholder="Please select"
+          onChange={props.setCategories}
+          tokenSeparators=","
+          className="dropdown"
+          showArrow
+        >
+          {
+            props.categories.map(category => <Option key={category} value={category}>{category}</Option>)
+          }
+        </Select>
 
-      <label>Organised By</label>
-      <Select
-        mode="multiple"
-        placeholder="Please select"
-        onChange={props.setOrganisers}
-        tokenSeparators=","
-        className="dropdown"
-      >
-        {
-          props.organiserTypes.map(category => <Option key={category} value={category}>{category}</Option>)
-        }
-      </Select>
-    </div>
+        <label>Organised By</label>
+        <Select
+          mode="multiple"
+          placeholder="Please select"
+          onChange={props.setOrganisers}
+          tokenSeparators=","
+          className="dropdown"
+          showArrow
+        >
+          {
+            props.organiserTypes.map(category => <Option key={category} value={category}>{category}</Option>)
+          }
+        </Select>
+      </div>
+      <Divider />
+    </>
   )
 }
