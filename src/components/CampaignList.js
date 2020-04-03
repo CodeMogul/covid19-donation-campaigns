c71a03cfe34e2813c6f2a0c837193661cb9f1060
Siddhesh Nachane <siddhesh.nachane@bizongo.com>
2020-04-04 01:04:00 +0530

    (feat): Update to card layout

--- src/components/CampaignList.js
+++ src/components/CampaignList.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react'
-import { Button, List } from 'antd';
+import { Button, List, Card } from 'antd';
 import Avatar from 'antd/es/avatar';
-// import List from 'antd/es/list';
-import Descriptions from 'antd/es/descriptions';
 import 'antd/es/list/style/css';
 import 'antd/es/avatar/style/css';
-import 'antd/es/descriptions/style/css';
 import 'antd/es/button/style/css';
+import 'antd/es/card/style/css';
 
 import './CampaignList.css';
+
+const Description = ({ label, title, style, children, ...others }) => (
+  <p style={{ margin: 0, paddingBottom: 8, color: 'black', ...style }} className="ellipsis" {...others}>
+    <span style={{ width: 96, display: 'inline-block' }}>{label} {': '}</span>
+    <span title={title} style={{ color: 'rgba(0, 0, 0, 0.65)' }}>{children}</span>
+  </p>
+)
 
 export default class CampaignList extends Component {
   getLinkText = (link, text, donationLink = false) => {
     if (link !== '#')
       return (
-      <a
-        href={link}
-        className={donationLink ? 'donation-link' : 'external-link'}
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        {text}
-      </a>);
+        <a
+          href={link}
+          className={donationLink ? 'donation-link' : 'external-link'}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          {text}
+        </a>);
     else return text;
   }
 
@@ -29,51 +34,75 @@ export default class CampaignList extends Component {
     return (
       <div>
         <List
-          itemLayout="vertical"
+          grid={{
+            gutter: [24, 8],
+            xs: 1,
+            sm: 2,
+            md: 2,
+            lg: 3,
+            xl: 4,
+            xxl: 4,
+          }}
           size="large"
           pagination={{
             onChange: page => {
               document.getElementById("main-content").scrollIntoView();
             },
-            pageSize: 5,
+            pageSize: 8,
           }}
           dataSource={this.props.listData}
           renderItem={({ initiator, ...item }) => (
             <List.Item
               key={item.name}
-              extra={(
-                <a className="donation-link" href={item.link} rel="noopener noreferrer" target="_blank">
-                  <img className="cover-image" alt={`${initiator.name} Banner`} src={item.image_url} />
-                </a>
-              )}
-              className="list-item"
             >
-              <List.Item.Meta
-                avatar={<Avatar src={item.avatar_url} />}
-                title={ this.getLinkText(item.link, item.name, true) }
-                description={item.description}
-              />
+              <Card
+                cover={
+                  <div style={{ position: "relative" }}>
+                    {item.tax_exemption && <span className="tax-tag">Tax Benefits</span>}
+                    <img className="cover-image" alt={`${initiator.name} Banner`} src={item.image_url} />
+                  </div>
+                }
+                className="card"
+              // actions={[
+              //   <SettingOutlined key="setting" />,
+              //   <EditOutlined key="edit" />,
+              //   <EllipsisOutlined key="ellipsis" />,
+              // ]}
+              >
+                <Card.Meta
+                  avatar={<Avatar src={item.avatar_url} />}
+                  title={this.getLinkText(item.link, item.name, true)}
+                />
+                <p className="card-meta">
+                  {item.description}
+                </p>
 
-              <div className="item-content-container">
-
-                <Descriptions column={2} bordered size="small" style={{ flexBasis: '85%' }}>
-                  <Descriptions.Item label="Initiative By">
-                    { this.getLinkText(initiator.link, initiator.name) }
-                  </Descriptions.Item>
-                  <Descriptions.Item label="NGO Partner">
-                    { this.getLinkText(item.partner_ngo.link, item.partner_ngo.name) }
-                  </Descriptions.Item>
-                  {/* <Descriptions.Item label="Platform Partner">
-                    { this.getLinkText(item.partner_platform.link, item.partner_platform.name) }
-                  </Descriptions.Item> */}
-                  <Descriptions.Item label="Tax Exempted u/s 80G">{item.tax_exemption ? "Yes" : "No"}</Descriptions.Item>
-                  <Descriptions.Item label="Category">{item.category}</Descriptions.Item>
+                  <Description label="Initiative By">
+                    {this.getLinkText(initiator.link, initiator.name)}
+                  </Description>
+                  <Description label="NGO Partner" title={item.partner_ngo.name}>
+                    {this.getLinkText(item.partner_ngo.link, item.partner_ngo.name)}
+                  </Description>
+                  {/*
+                      <Descriptions.Item label="Platform Partner">
+                        { this.getLinkText(item.partner_platform.link, item.partner_platform.name) }
+                      </Descriptions.Item>
+                    */}
+                  <Description label="Category" title={item.category} >
+                    {item.category}
+                  </Description>
                   {/* <Descriptions.Item label="Location">{item.location}</Descriptions.Item> */}
-                </Descriptions>
 
-                <Button type="primary" className="donation-link" href={item.link} target="_blank">Know More</Button>
-              </div>
-
+                  <Button type="primary"
+                    className="donation-link"
+                    href={item.link}
+                    target="_blank"
+                    shape="round"
+                    block
+                  >
+                    Know More
+                  </Button>
+              </Card>
             </List.Item>
           )}
         />
